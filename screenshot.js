d7a33b0de3de4df716805060c0103cfb61d81725
ashishmg <ashishmangla1991@gmail.com>
2020-05-31 13:17:03 +0530

    add js for setViewposrt

--- screenshot.js
+++
@@ -1,9 +0,0 @@
-const puppeteer = require('puppeteer');
-(async () => {
-  const browser = await puppeteer.launch(); //launch Browser which is "Headless"
-  const page = await browser.newPage(); // open a new page
-  await page.goto('https://www.google.co.in/'); // enter url in page
-  await page.screenshot({path: 'screenshot.png'});// takes a screenshot
-  await browser.close(); //close the browser
-})();
-
